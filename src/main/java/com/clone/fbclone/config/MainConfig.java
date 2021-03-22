4710b1f89f84d066d409b16166033dbeeee3c3f4
Aminul-hoq <61986445+Aminul-hoq@users.noreply.github.com>
2021-03-22 15:38:17 +0600

    dependency and config added for heroku database

---
+++ src/main/java/com/clone/fbclone/config/MainConfig.java
@@ -0,0 +1,26 @@
+package com.clone.fbclone.config;
+
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+import java.net.URISyntaxException;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+@Configuration
+public class MainConfig {
+
+    @Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
